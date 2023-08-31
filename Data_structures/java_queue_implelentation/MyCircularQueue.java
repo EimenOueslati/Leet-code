df6c80d6db8e31a7ce702557b867c0f542eab31b
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-08-31 15:39:33 +0200

    Added exercise description in the MyCircularQueue class

--- Data_structures/java_queue_implelentation/MyCircularQueue.java
+++ Data_structures/java_queue_implelentation/MyCircularQueue.java
@@ -1,3 +1,15 @@
+/**
+ * Implement the MyCircularQueue class:
+
+    -MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+    -int Front() Gets the front item from the queue. If the queue is empty, return -1.
+    -int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+    -boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+    -boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+    -boolean isEmpty() Checks whether the circular queue is empty or not.
+    -boolean isFull() Checks whether the circular queue is full or not.
+ */
+
 public class MyCircularQueue {
     private int head;
     private int tail;
@@ -79,6 +91,7 @@ public class MyCircularQueue {
         {
             return true;
         }
+        return false;
     
     } 
 }  
