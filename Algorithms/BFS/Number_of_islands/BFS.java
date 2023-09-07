82a03215d87f76a5d6a974ea4bb0940a1fc9d630
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-09-07 11:44:10 +0200

    Switched back to recursive BFS function for simpler code

--- Algorithms/BFS/Number_of_islands/BFS.java
+++ Algorithms/BFS/Number_of_islands/BFS.java
@@ -6,147 +6,37 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BFS {
-    public int numIslands(char[][] grid) {
-        int dim1 = grid.length;
-        int dim2 = grid[1].length;
-        Queue<int[]> queue = new LinkedList<>();;
-        boolean[][] visted1 = new boolean[dim1][dim2];
-        ArrayList<int[]> visited = new ArrayList<int[]>();
-        //for (int i = 0; i < visited.length; i++) {
-          //  Arrays.fill(visited[i], -1);
-        //}
-        //int visitedIndex = 0;
-        int islandNumber = 0;
-        
+    
+private int n;
+private int m;
 
-         for(int i = 0; i < dim1; i++)
-         {
-            for(int y = 0; y < dim2; y++)
-            {
-                int[] pos = {i, y};
-                if(Character.compare(grid[i][y], '1') == 0 && !visted1[i][y])
-                {
-                    queue.offer(pos);
-                    //BFsearch(grid, visited, visitedIndex, queue);
-                    //iterativeBFS(grid, visited, queue);
-                    iterativeBFS(grid, visted1, queue);
-                    islandNumber++;
-                    
-
-                }
-
-
-                    
-                }
+public int numIslands(char[][] grid) {
+    int count = 0;
+    n = grid.length;
+    if (n == 0) return 0;
+    m = grid[0].length;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == '1') {
+                DFsearch(grid, i, j);
+                ++count;
             }
-
-         
-
-        return islandNumber;
+    }    
+    return count;
+}
+    
+    private void DFsearch(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFsearch(grid, i + 1, j);
+        DFsearch(grid, i - 1, j);
+        DFsearch(grid, i, j + 1);
+        DFsearch(grid, i, j - 1);
     }
     
-    private boolean doesntContainsArr(ArrayList<int[]> search, int[] val)
-    {
-        for(int i = 0; i < search.size(); i++)
-        {
-            if(Arrays.equals(search.get(i), val))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private void BFsearch(char[][] grid, ArrayList<int[]> log, Queue<int[]> q)
-    {
-        if(!q.isEmpty())
-        {
-            int[] currPos = q.peek();
-            if (currPos[0] + 1 < grid.length && Character.compare(grid[currPos[0] +1][currPos[1]], '1') == 0 && doesntContainsArr(log, new int[]{currPos[0] +1, currPos[1]}))
-            {
-                q.offer(new int[]{currPos[0] +1, currPos[1]});
-            }
+   
 
-            if (currPos[0] > 0 && Character.compare(grid[currPos[0] -1][currPos[1]], '1') == 0 && doesntContainsArr(log, new int[]{currPos[0] -1, currPos[1]}))
-            {
-                q.offer(new int[]{currPos[0] -1, currPos[1]});
-            }
-
-            if (currPos[1] > 0 && Character.compare(grid[currPos[0]][currPos[1] - 1], '1') == 0 && doesntContainsArr(log, new int[]{currPos[0] ,currPos[1] - 1}))
-            {
-                q.offer(new int[]{currPos[0] ,currPos[1] - 1});
-            }
-
-            if (currPos[1] + 1 < grid[0].length && Character.compare(grid[currPos[0] ][currPos[1] + 1], '1') == 0 && doesntContainsArr(log, new int[]{currPos[0] ,currPos[1] + 1}))
-            {
-                q.offer(new int[]{currPos[0] ,currPos[1] + 1});
-            }
-            log.add(currPos);
-            q.poll();
-            BFsearch(grid, log, q);
-
-
-
-        }
-
-    }
-
-    private boolean containsArr(ArrayList<int[]> search, int x, int y) {
-        for (int[] arr : search) {
-            if (arr[0] == x && arr[1] == y) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void iterativeBFS(char[][] grid, ArrayList<int[]> log, Queue<int[]> q){
-        int[][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
-        while(!q.isEmpty())
-        {
-
-            int[] currPos = q.poll();
-            for(int[] newpos : direction)
-            {
-                int newX = currPos[0] + newpos[0];
-                int newY = currPos[1] + newpos[1];
-
-                if(newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length
-                && grid[newX][newY] == '1' && !containsArr(log, newX, newY))
-                {
-                    q.offer(new int[]{newX, newY});
-                }
-            }
-            log.add(currPos);
-
-
-        }
-    }
-
-
-    private void iterativeBFS(char[][] grid, boolean[][] visted1, Queue<int[]> q){
-        int[][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
-        while(!q.isEmpty())
-        {
-
-            int[] currPos = q.poll();
-            for(int[] newpos : direction)
-            {
-                int newX = currPos[0] + newpos[0];
-                int newY = currPos[1] + newpos[1];
-
-                if(newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length
-                && grid[newX][newY] == '1' && !visted1[newX][newY])
-                {
-                    q.offer(new int[]{newX, newY});
-                }
-            }
-            visted1[currPos[0]][currPos[1]] = true;
-
-
-        }
-    }
-
+   
 
 }
 
