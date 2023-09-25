41efb464102a9656fef41fa3d98d35b8cfda8918
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-09-25 12:08:05 +0200

    Added documentation to the solution class in the perfect squares folder

--- Algorithms/BFS/Perfect_squares/Solution.java
+++ Algorithms/BFS/Perfect_squares/Solution.java
@@ -1,18 +1,28 @@
 package Algorithms.BFS.Perfect_squares;
 import java.util.LinkedList;
 import java.util.Queue;
-
+/* 
+ * This class in the solution for the leetcode proble "Perfect squares"
+ * Given an integer n, return the least number of perfect square numbers that sum to n.
+ * A perfect square is an integer that is the square of an integer; in other words, 
+ * it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+ * 
+ * Constraints:
+ *      1 <= n <= 10^^4
+ */
 public class Solution {
     public int  numSquares(int n)
     {
-        int steps = 1;
-        Queue<Integer> q = new LinkedList();
+        int steps = 1; // variable to keep track of how many steps we curent have
+        Queue<Integer> q = new LinkedList(); // A queue for the BFS search
+        
+        // Get the largest perfect square that is smaller than the target int
         Double i = Math.floor(Math.sqrt(n));
         for(int j = 1; j <= i; j++)
         {
             q.offer(j*j);
         }
-        if(q.contains(n))
+        if(q.contains(n)) // In case the targer is a perfect square
         {
             return 1;
         }
