a6291421df9331dd8486d58f6f842e868e133d61
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-09-25 13:13:08 +0200

    Switched to a BITWISE operation solution using XOR operations for a much faster solution

--- Algorithms/BFS/Find_the_difference/Solution.java
+++ Algorithms/BFS/Find_the_difference/Solution.java
@@ -1,30 +1,10 @@
 package Algorithms.BFS.Find_the_difference;
 
-public class Solution {
+class Solution {
     public char findTheDifference(String s, String t) {
-        StringBuilder sb1 = new StringBuilder(s);
-        StringBuilder sb2 = new StringBuilder(t);
-        int[] Sarr = new int[26];
-        int[] Tarr = new int[26];
-        
-        for(int i = 0; i < s.length(); i++)
-        {
-           Sarr[sb1.charAt(i) - 97]++;
-        }
-
-        for(int i = 0; i < t.length(); i++)
-        {
-           Tarr[sb2.charAt(i) - 97]++;
-        }
-
-        for(int i = 0; i < 26; i++)
-        {
-            if(Sarr[i] != Tarr[i])
-            {
-                return (char)(i + 97);
-            }
-        }
-        return 'a';
+        char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;
     }
-
 }
