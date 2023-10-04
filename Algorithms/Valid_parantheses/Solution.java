cf1849794dcb8b9d7b93a26d1938e47ae602c003
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-04 16:13:01 +0200

    Added some documentation

--- Algorithms/Valid_parantheses/Solution.java
+++ Algorithms/Valid_parantheses/Solution.java
@@ -6,6 +6,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+
+/*
+ * This class in the solution for the leetcode proble "Open the lock":
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * 
+ * Constraints:
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+
+ */
 public class Solution {
 
   Map<Character, Character> map = new HashMap<>();
