ab2554043c019249b1bc00f7679267b13a2b7208
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-06 15:19:40 +0200

    Added some documentation

--- Algorithms/DFs/Number_or_islands/Solution.java
+++ Algorithms/DFs/Number_or_islands/Solution.java
@@ -2,6 +2,17 @@ package Algorithms.DFs.Number_or_islands;
 
 import java.util.Stack;
 
+/*
+ * This class in the solution for the leetcode proble "Numbre of islands":
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ * 
+ * Constraints:
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] is '0' or '1'.
+ */
 public class Solution {
     public int numIslands(char[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
