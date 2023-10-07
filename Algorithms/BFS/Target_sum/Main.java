a3be3489f1344c0e20c90e553b436fa133c48738
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-07 11:38:42 +0200

    Fixed a bug that happens when all the lements besides the first one are 0

--- Algorithms/BFS/Target_sum/Main.java
+++ Algorithms/BFS/Target_sum/Main.java
@@ -2,7 +2,7 @@ package Algorithms.BFS.Target_sum;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,0};
+        int[] arr = new int[]{0,0,0,0,0,0,0,0,1};
         int target = 1;
         Solution sol = new Solution();
         System.out.println(sol.findTargetSumWays(arr, target));
--- Algorithms/BFS/Target_sum/Solution.java
+++ Algorithms/BFS/Target_sum/Solution.java
@@ -7,15 +7,14 @@ public class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         if(nums.length == 0) return 0;
         int combinations = 0;
+        if(nums.length == 1 && (nums[0] == target || -nums[0] == target)) return 1;
         Queue<Integer> q = new LinkedList<>();
-        if(nums[0] == target) combinations++;
-        if(-nums[0] == target) combinations++;
         q.add(nums[0]);
         q.add(-nums[0]);
         for(int i = 1; i < nums.length; i++)
         {
             
-            for(int j = 0; j < i*2; j++)
+            for(int j = 0; j < (int) Math.pow(2, i); j++)
             {
                 int curr = q.poll();
                 int elem1 = curr + nums[i];
