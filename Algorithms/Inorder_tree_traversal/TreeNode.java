ae9bbb1f2912d8f4367a91a3f10e22974c39305c
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-08 23:05:41 +0200

    Implemented the tree node class

---
+++ Algorithms/Inorder_tree_traversal/TreeNode.java
@@ -0,0 +1,14 @@
+package Algorithms.Inorder_tree_traversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
