aa02f9c6a20da0329b41895a84f878f7e67b4db9
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-08 18:41:38 +0200

    Experimenting to recure runtime

--- Algorithms/BFS/Target_sum/Main.java
+++ Algorithms/BFS/Target_sum/Main.java
@@ -2,8 +2,8 @@ package Algorithms.BFS.Target_sum;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{0,0,0,0,0,0,0,0,1};
-        int target = 1;
+        int[] arr = new int[]{1,1,1,1,1};
+        int target = 3;
         Solution sol = new Solution();
         System.out.println(sol.findTargetSumWays(arr, target));
     }
--- Algorithms/BFS/Target_sum/Solution.java
+++ Algorithms/BFS/Target_sum/Solution.java
@@ -5,26 +5,47 @@ import java.util.Queue;
 
 public class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        if(nums.length == 0) return 0;
+        int arrlen = nums.length;
+        if(arrlen == 0) return 0;
         int combinations = 0;
-        if(nums.length == 1 && (nums[0] == target || -nums[0] == target)) return 1;
+        if(arrlen== 1 && (nums[0] == target || -nums[0] == target)) return 1;
+        if(arrlen == 1 && (nums[0] != target || -nums[0] != target)) return 0;
         Queue<Integer> q = new LinkedList<>();
         q.add(nums[0]);
         q.add(-nums[0]);
-        for(int i = 1; i < nums.length; i++)
+        int elem1, elem2, curr;
+        for(int i = 1; i < arrlen - 1; i++)
         {
-            
-            for(int j = 0; j < (int) Math.pow(2, i); j++)
+            int iterations = (int) Math.pow(2, i);
+            for(int j = 0; j < iterations; j++)
             {
-                int curr = q.poll();
-                int elem1 = curr + nums[i];
-                int elem2 = curr - nums[i];
-                if(elem1 == target) combinations++;
-                if(elem2 == target) combinations++;
-                q.add(elem1);
-                q.add(elem2);
+                curr = q.poll();
+                elem1 = curr + nums[i];
+                elem2 = curr - nums[i];
+                if(i == arrlen - 2)
+                {
+                    if(elem1 + nums[i + 1] == target) combinations++;
+                    if(elem1 - nums[i + 1] == target) combinations++;
+                    if(elem2 + nums[i + 1] == target) combinations++;
+                    if(elem2 - nums[i + 1] == target) combinations++;
+                }else{
+                    q.add(elem1);
+                    q.add(elem2);
+                }
+                
             }
         }
+        /*int index = arrlen - 1;
+        int iterations = (int) Math.pow(2, index);
+        for(int j = 0; j < iterations; j++)
+        {
+            curr = q.poll();
+            elem1 = curr + nums[index];
+            elem2 = curr - nums[index];
+            if(elem1 == target) combinations++;
+            if(elem2 == target) combinations++;
+        }
+        */
         return combinations;
     }
 }
