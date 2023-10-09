19cbf047b5dca6024ffbc395cf754a981f253020
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-09 10:34:19 +0200

    Added a constructor to the class

---
+++ Data_structures/Java_queue_stack_implementation/MyStack.java
@@ -0,0 +1,14 @@
+package Data_structures.Java_queue_stack_implementation;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+}
