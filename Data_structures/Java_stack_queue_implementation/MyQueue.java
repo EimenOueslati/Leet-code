39bdca178e744d7a95acc9e05999391ec22fa4ae
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-09 10:00:46 +0200

    Implemented the push method

--- Data_structures/Java_stack_queue_implementation/MyQueue.java
+++ Data_structures/Java_stack_queue_implementation/MyQueue.java
@@ -12,6 +12,15 @@ public class MyQueue {
         st2 = new Stack<>();
     }
 
+     public void push(int x) {
+        if(st1.empty()) st1.add(x);
+        else{
+            swap(st1, st2);
+            st2.add(x);
+            swap(st2, st1);
+        }
+    }
+
     private void swap(Stack<Integer> from, Stack<Integer> to)
     {   
         while(!from.isEmpty())
