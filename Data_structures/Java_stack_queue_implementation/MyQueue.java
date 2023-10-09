9179e6b369c97520e140a56dd255e7baeed02342
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-09 10:00:15 +0200

    Implemented the swap helper method

--- Data_structures/Java_stack_queue_implementation/MyQueue.java
+++ Data_structures/Java_stack_queue_implementation/MyQueue.java
@@ -1,5 +1,24 @@
 package Data_structures.Java_stack_queue_implementation;
 
+import java.util.Stack;
+
 public class MyQueue {
-    
+
+    private Stack<Integer> st1;
+    private Stack<Integer> st2;
+
+    public MyQueue() {
+        st1 = new Stack<>();
+        st2 = new Stack<>();
+    }
+
+    private void swap(Stack<Integer> from, Stack<Integer> to)
+    {   
+        while(!from.isEmpty())
+        {
+            to.add(from.pop());
+        }
+        
+
+    }
 }
