d57698eb5fadc676bbe8071dd52cb7409ae2f5d9
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-09 20:54:13 +0200

    trying another loop solution

--- Algorithms/Decoding_string/Main.java
+++ Algorithms/Decoding_string/Main.java
@@ -2,7 +2,7 @@ package Algorithms.Decoding_string;
 
 public class Main {
     public static void main(String[] args) {
-        String test = "3[a2[c]]";
+        String test = "2[abc]3[cd]ef";
         Solution sol = new Solution();
         System.out.println(sol.decodeString(test));
     }
--- Algorithms/Decoding_string/Solution.java
+++ Algorithms/Decoding_string/Solution.java
@@ -1,20 +1,78 @@
 package Algorithms.Decoding_string;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Solution {
-    private String answer;
+    
     public String decodeString(String s) {
-        answer = "";
-        String[] arr = s.split("]");
-        for(String str : arr)
+        StringBuilder str = new StringBuilder(s);
+        Stack<String> charq = new Stack<>();
+        Stack<Integer> intst = new Stack<>();
+        int status = 0;
+        String answer = "";
+        String ret = "";
+        for(int i = 0; i < s.length(); i++)
         {
-            StringBuilder temp = new StringBuilder(str.substring(2));
-            for(int i = 0; i < str.charAt(0) - '0'; i++)
+            if(str.charAt(i) == '[') status++;
+            else if(str.charAt(i) == ']')
             {
-                answer += temp;
-            }
+                status--;
+                int num = intst.pop();
+                String ch = charq.pop() + answer;
+                String temp = "";
+                for(int j = 0; j < num; j++) temp += ch;
+                answer = temp;
+                if(status == 0 && answer.length() != 0)
+                {
+                    ret += answer;
+                    answer = "";
+                }
+            }else if(Character.isDigit(str.charAt(i)))
+            {
+                    //intst.add(str.charAt(i) - '0');
+                    int k = 0;
+                    String temp = "";
+                    while(i + k < str.length() && Character.isDigit(str.charAt(i + k)))
+                    {
+                        temp = temp + str.charAt(i + k);
+                        k++;
+                    }
+                    i = i+k-1;
+                    intst.add(Integer.parseInt(temp));
+            } 
+            else
+            {
+                if(status == 0)
+                {
+                    int k = 0;
+                    String temp = "";
+                    while(i + k < str.length() && Character.isLetter(str.charAt(i + k)))
+                    {
+                        temp = temp + str.charAt(i + k);
+                        k++;
+                    }
+                    i = i+k-1;
+                    ret += temp;
+
+                }else
+                {
+                    int k = 0;
+                    String temp = "";
+                    while(i < str.length() && Character.isLetter(str.charAt(i + k)))
+                    {
+                        temp = temp + str.charAt(i + k);
+                        k++;
+                    }
+                    i = i+k-1;
+                    charq.add(temp);
+                }   
+                
+            } 
         }
 
-        return answer;
+        return ret;
         
     }
 }
