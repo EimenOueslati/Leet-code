58b1ae6e6aff1846c984ca50d54ecb36172128d0
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-09 16:34:23 +0200

    First implementation of the solution for Decoding string problem

--- Algorithms/Decoding_string/Solution.java
+++ Algorithms/Decoding_string/Solution.java
@@ -1,5 +1,20 @@
 package Algorithms.Decoding_string;
 
 public class Solution {
-    
+    private String answer;
+    public String decodeString(String s) {
+        answer = "";
+        String[] arr = s.split("]");
+        for(String str : arr)
+        {
+            StringBuilder temp = new StringBuilder(str.substring(2));
+            for(int i = 0; i < str.charAt(0) - '0'; i++)
+            {
+                answer += temp;
+            }
+        }
+
+        return answer;
+        
+    }
 }
