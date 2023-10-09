1686007bdf4014ed63ec273532bf2e5a6829d82d
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-09 11:19:01 +0200

    Updated the main testing class

--- Data_structures/Java_queue_stack_implementation/Main.java
+++ Data_structures/Java_queue_stack_implementation/Main.java
@@ -2,13 +2,13 @@ package Data_structures.Java_queue_stack_implementation;
 
 public class Main {
     public static void main(String[] args) {
-        MyStack obj = new MyStack();
-        obj.push(19);
+        MyStack2 obj = new MyStack2();
+        obj.push(1);
+        obj.push(2);
         int param_1 = obj.top();
         int param_2 = obj.pop();
-        int param_3 = obj.top();
         boolean param_4 = obj.empty();
-        System.out.println(param_1 + " " + param_2 + " " + param_3 + " " + param_4);
+        System.out.println(param_1 + " " + param_2 + " " + param_4);
     }
     
 }
