117dfa3b1ae5e7b5312d990de1bb722eacb969a9
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-10 17:44:02 +0200

    Created and implemented the solution for the flood fill problem

---
+++ Algorithms/Flood_fill/Solution.java
@@ -0,0 +1,25 @@
+package Algorithms.Flood_fill;
+
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+       if(sr > 0 && image[sr][sc] == image[sr - 1][sc])
+       {
+            floodFill(image, sr - 1, sc, color);
+       }
+       if(sc < image[0].length - 1 && image[sr][sc] == image[sr][sc + 1])
+       {
+            floodFill(image, sr, sc + 1, color);
+       }
+       if(sr < image.length - 1 && image[sr][sc] == image[sr + 1][sc])
+       {
+            floodFill(image, sr + 1, sc, color);
+       }
+        if(sc > 0 && image[sr][sc] == image[sr][sc - 1])
+       {
+            floodFill(image, sr, sc - 1, color);
+       }
+
+       image[sr][sc] = color;
+       return image;
+    }
+}
