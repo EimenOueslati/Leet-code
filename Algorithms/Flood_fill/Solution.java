c2fe20f38f76b6a1a3a3b081ad018e3d98a353d5
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-10 17:47:48 +0200

    fixed a bug in the recursive function

--- Algorithms/Flood_fill/Solution.java
+++ Algorithms/Flood_fill/Solution.java
@@ -11,19 +11,19 @@ class Solution {
     {
          if(sr > 0 && image[sr][sc] == image[sr - 1][sc])
        {
-            floodFill(image, sr - 1, sc, color);
+            fill(image, sr - 1, sc, color);
        }
        if(sc < image[0].length - 1 && image[sr][sc] == image[sr][sc + 1])
        {
-            floodFill(image, sr, sc + 1, color);
+            fill(image, sr, sc + 1, color);
        }
        if(sr < image.length - 1 && image[sr][sc] == image[sr + 1][sc])
        {
-            floodFill(image, sr + 1, sc, color);
+            fill(image, sr + 1, sc, color);
        }
         if(sc > 0 && image[sr][sc] == image[sr][sc - 1])
        {
-            floodFill(image, sr, sc - 1, color);
+            fill(image, sr, sc - 1, color);
        }
 
        image[sr][sc] = color;
