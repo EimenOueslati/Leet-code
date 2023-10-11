0cfc8ff2cc39552d61df96ea4855b93337600b88
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-11 10:48:28 +0200

    Created the main class for testing and debugging

---
+++ Algorithms/OI_matrix/Main.java
@@ -0,0 +1,9 @@
+package Algorithms.OI_matrix;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] mat = new int[][]{{0,0,0},{0,1,0},{0,0,0}};
+        Solution slo = new Solution();
+        slo.updateMatrix(mat);
+    }
+}
