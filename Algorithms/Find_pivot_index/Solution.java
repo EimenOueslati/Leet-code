70283ea601fadfa45f69438eca58af57e85401d6
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 16:49:58 +0200

    Created the pivotIndex method

--- Algorithms/Find_pivot_index/Solution.java
+++ Algorithms/Find_pivot_index/Solution.java
@@ -1,5 +1,15 @@
 package Algorithms.Find_pivot_index;
 
 public class Solution {
+    public int pivotIndex(int[] nums) {
+        int sumRight = 0;
+        for(int i : nums)
+        {
+            sumRight += i;
+        }
+        if(sumRight - nums[0] == nums[0]) return 0;
+        int ret = findPiv(1,nums[0], sumRight - nums[0], nums);
+        return ret;
+    }
     
 }
