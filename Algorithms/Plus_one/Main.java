89f63545b5852b855b5d76a23b6c74373218a1a4
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 21:43:04 +0200

    Created the main class for testing and debuging

---
+++ Algorithms/Plus_one/Main.java
@@ -0,0 +1,10 @@
+package Algorithms.Plus_one;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] test = {9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9};
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.plusOne(test)));
+    }
+}
