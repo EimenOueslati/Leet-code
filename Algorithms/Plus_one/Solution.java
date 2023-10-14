b1333379556379e6f1e213fd226581d255240181
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 21:45:14 +0200

    Added some documentation to the solution for Plus One problem

--- Algorithms/Plus_one/Solution.java
+++ Algorithms/Plus_one/Solution.java
@@ -1,5 +1,17 @@
 package Algorithms.Plus_one;
 
+
+/*
+ * This class in the solution for the leetcode problem "Largest number at least twice the others":
+ * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+ * The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+ * Increment the large integer by one and return the resulting array of digits.
+ * 
+ * Constraints:
+ * 1 <= digits.length <= 100
+ * 0 <= digits[i] <= 9
+ * digits does not contain any leading 0's.
+ */
 public class Solution {
     public int[] plusOne(int[] digits) {
         int[] ret = addOne(digits.length - 1, digits);
