7b9ec76a4201d90e9ce3cead477acc47609a7cf8
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 18:08:36 +0200

    Created the dominanIndex method in the solution class

--- Algorithms/Largest_number_at_least_twice_of_others/Solution.java
+++ Algorithms/Largest_number_at_least_twice_of_others/Solution.java
@@ -1,5 +1,22 @@
 package Algorithms.Largest_number_at_least_twice_of_others;
 
 public class Solution {
-    
+    public int dominantIndex(int[] nums) {
+        int largest = 0;
+        int secondLatgest = -1;
+        int currInd = 0;
+        for(int i : nums)
+        {
+            if(i > nums[largest])
+            {
+                secondLatgest = largest;
+                largest = currInd;
+                currInd++;
+            }
+        }
+
+        if(secondLatgest == -1) return -1;
+        else if(nums[largest] >= nums[secondLatgest] *2) return largest;
+        else return -1;
+    }
 }
