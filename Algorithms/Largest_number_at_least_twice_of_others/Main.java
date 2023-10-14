7869e246b884d66be1a4c77e7f99032d53b6261c
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 18:52:31 +0200

    Switched to different solution

--- Algorithms/Largest_number_at_least_twice_of_others/Main.java
+++ Algorithms/Largest_number_at_least_twice_of_others/Main.java
@@ -2,7 +2,7 @@ package Algorithms.Largest_number_at_least_twice_of_others;
 
 public class Main {
     public static void main(String[] args) {
-        int[] test = {3,6,1,0};
+        int[] test = {3,0,0,2};
         Solution sol = new Solution();
         System.out.println(sol.dominantIndex(test));
     }
--- Algorithms/Largest_number_at_least_twice_of_others/Solution.java
+++ Algorithms/Largest_number_at_least_twice_of_others/Solution.java
@@ -3,21 +3,23 @@ package Algorithms.Largest_number_at_least_twice_of_others;
 public class Solution {
     public int dominantIndex(int[] nums) {
         int largest = 0;
-        int secondLatgest = -1;
-        int currInd = 0;
+        int index = 0;
         for(int i : nums)
         {
-            if(i > nums[largest])
-            {
-                currInd++;
-                secondLatgest = largest;
-                largest = currInd;
-                
-            }
+            if(i > nums[largest]) largest = index;
+            index++;
         }
-
-        if(secondLatgest == -1) return -1;
-        else if(nums[largest] >= nums[secondLatgest] *2) return largest;
-        else return -1;
+        index = 0;
+        for(int i : nums)
+        {
+            if( index == largest)
+            {
+                index++;
+                continue;
+            } 
+            else if(nums[largest] < i * 2) return -1;
+            index++;
+        }
+        return largest;
     }
 }
