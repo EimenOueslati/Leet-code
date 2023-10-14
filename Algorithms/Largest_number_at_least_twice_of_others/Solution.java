2e9a2d4f01328d4960dad68e0cc36bb7366bafe0
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 18:58:56 +0200

    Added some documentation to the solution for The Largest Number At Least Twice of others problem

--- Algorithms/Largest_number_at_least_twice_of_others/Solution.java
+++ Algorithms/Largest_number_at_least_twice_of_others/Solution.java
@@ -1,5 +1,16 @@
 package Algorithms.Largest_number_at_least_twice_of_others;
 
+/*
+ * This class in the solution for the leetcode problem "Largest number at least twice the others":
+ * You are given an integer array nums where the largest integer is unique.
+ * Determine whether the largest element in the array is at least twice as much
+ *  as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+ * 
+ * Constraints:
+ * 2 <= nums.length <= 50
+ * 0 <= nums[i] <= 100
+ * The largest element in nums is unique.
+ */
 public class Solution {
     public int dominantIndex(int[] nums) {
         int largest = 0;
