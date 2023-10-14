55de5ad1c023a24bd21b4a170f452f3bfe402638
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 18:57:10 +0200

    Added some documentation to the solution for Flood Fill problem

--- Algorithms/Flood_fill/Solution.java
+++ Algorithms/Flood_fill/Solution.java
@@ -1,5 +1,25 @@
 package Algorithms.Flood_fill;
 
+
+
+/*
+ * This class in the solution for the leetcode problem "Flood fill":
+ * An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+ * You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+ * To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as 
+ * the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color 
+ * of all of the aforementioned pixels with color.
+ * Return the modified image after performing the flood fill.
+ * 
+ * 
+ * Constraints:
+ * m == image.length
+ * n == image[i].length
+ * 1 <= m, n <= 50
+ * 0 <= image[i][j], color < 216
+ * 0 <= sr < m
+ * 0 <= sc < n
+ */
 class Solution {
     boolean[][] visited;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
