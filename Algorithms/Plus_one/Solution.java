5c7468a9e77ae783c461ed71479a7a65c3076cf3
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-14 21:39:45 +0200

    Created the plus one function in the solutoin class

---
+++ Algorithms/Plus_one/Solution.java
@@ -0,0 +1,30 @@
+package Algorithms.Plus_one;
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int[] ret = addOne(digits.length - 1, digits);
+        return  ret;
+    }
+
+    private int[] addOne(int index, int[] digits)
+    {
+        if(digits[index] != 9)
+        {
+          digits[index]++;
+          return digits;
+        }else
+        {
+            if(index == 0)
+            {
+                int[] arr = new int[digits.length+1];
+                System.arraycopy(digits, 0, arr, 2, digits.length);
+                arr[1] = 0;
+                arr[0] = 1;
+                return arr;
+            }else{
+                digits[index] = 0;
+                return addOne(index -1, digits);
+            }
+        }
+    }
+}
