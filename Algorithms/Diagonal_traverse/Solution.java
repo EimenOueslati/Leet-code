53a8ec118d3f23e307b7b249a60254bab4374216
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-15 15:16:01 +0200

    Created the recursive function traverseDiag

--- Algorithms/Diagonal_traverse/Solution.java
+++ Algorithms/Diagonal_traverse/Solution.java
@@ -1,11 +1,66 @@
 package Algorithms.Diagonal_traverse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int[] asnwer = new int[mat.length * mat[0].length];
-        trverseDiag(0,mat,true);
-        return asnwer;
+        int[] ans = new int[mat.length * mat[0].length];
+        List<Integer> answer = new ArrayList<>();
+        trverseDiag(new int[]{0, 0},mat, answer,true);
+
+        for(int i = 0; i < answer.size(); i++)
+        {
+            ans[i] = answer.get(i);
+        }
+       
+        return ans;
     }
 
-    
+    private void trverseDiag(int[] startpos, int[][] source, List<Integer> dest, boolean up)
+    {
+        
+        int x = startpos[0];
+        int y = startpos[1];
+        if(x == source.length -1 && y == source[0].length - 1) 
+        {
+            dest.add(source[x][y]);
+            return;
+        }
+        dest.add(source[x][y]);
+        if(up)
+        {
+            while(x > 0 && y < source[0].length - 1)
+            {
+                x--;
+                y++;
+                dest.add(source[x][y]);
+            }
+            if(y != source[0].length - 1)
+            {
+                trverseDiag(new int[]{x, y +1}, source, dest, false);
+            }else
+            {
+                trverseDiag(new int[]{x + 1, y}, source, dest, false);
+            }
+        }
+        else
+        {
+            while(x < source.length - 1  && y > 0)
+            {
+                x++;
+                y--;
+                dest.add(source[x][y]);
+            }
+            if(x != source.length - 1)
+            {
+                trverseDiag(new int[]{x + 1, y}, source, dest, true);
+            }else
+            {
+                trverseDiag(new int[]{x , y + 1}, source, dest, true);
+            }
+        }
+
+    }
+
 }
