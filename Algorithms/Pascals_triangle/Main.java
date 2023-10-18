746067465785f17a360a2b01cfbd3f252e753e5f
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-18 23:44:48 +0200

    Created a simple test for the Solution class for the pascal's triangle problem

---
+++ Algorithms/Pascals_triangle/Main.java
@@ -0,0 +1,9 @@
+package Algorithms.Pascals_triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        int rows = 5;
+        Solution sol = new Solution();
+        System.out.println(sol.generate(rows));
+    }
+}
