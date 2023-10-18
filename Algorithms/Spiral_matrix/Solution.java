095dcb9350f05da8e2a863503abfed4c34d167d6
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-18 16:09:12 +0200

    Fixed a bug in the recursive function where it would try to access unvalid elemets in the array

--- Algorithms/Spiral_matrix/Solution.java
+++ Algorithms/Spiral_matrix/Solution.java
@@ -7,11 +7,11 @@ public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> answer = new LinkedList<>();
         boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-        addSpiral(0, 0, visited, matrix,answer);
+        addSpiral(0, 0, visited, matrix,answer, 0);
         return answer;
     }
 
-    private void addSpiral(int x, int y, boolean[][] visited, int[][]  matrix, List<Integer> answer)
+    /*private void addSpiral(int x, int y, boolean[][] visited, int[][]  matrix, List<Integer> answer)
     {
         answer.add(matrix[x][y]);
         visited[x][y] = true;
@@ -29,6 +29,31 @@ public class Solution {
             addSpiral(x - 1, y, visited, matrix, answer);
         }else{
             return;
+        }
+    }*/
+    
+    private void addSpiral(int x, int y, boolean[][] visited, int[][]  matrix, List<Integer> answer, int dir)
+    {
+        answer.add(matrix[x][y]);
+        visited[x][y] = true;
+        if((x == 0 || visited[x - 1][y]) && (y == matrix[0].length - 1 || visited[x][y + 1]) && (x == matrix.length - 1 || visited[x + 1][y]) && (y == 0 || visited[x][y - 1])) return;
+        if( dir == 0 )
+        {
+            if(y < matrix[0].length - 1 && !visited[x][y + 1]) addSpiral(x, y + 1, visited, matrix, answer, 0);
+            else addSpiral(x + 1, y, visited, matrix, answer, 1);
+            
+        }else if( dir == 1)
+        {
+            if(x < matrix.length - 1 && !visited[x + 1][y]) addSpiral(x + 1, y, visited, matrix, answer, 1);
+            else addSpiral(x , y - 1, visited, matrix, answer, 2);
+        }else if(dir == 2)
+        {
+            if(y > 0 && !visited[x][y - 1]) addSpiral(x , y - 1, visited, matrix, answer, 2);
+            else addSpiral(x - 1, y, visited, matrix, answer, 3);
+        }else if( dir == 3)
+        {
+            if (x > 0 && !visited[x - 1][y]) addSpiral(x - 1, y, visited, matrix, answer, 3);
+            else addSpiral(x, y + 1, visited, matrix, answer, 0);
         }
     }
 
