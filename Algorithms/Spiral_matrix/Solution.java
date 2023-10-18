ce69dc2ab860c739fae628c7b23ae3cafab9f4c1
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-18 16:14:49 +0200

    Added the problem desciption to the spiral matrix problem

--- Algorithms/Spiral_matrix/Solution.java
+++ Algorithms/Spiral_matrix/Solution.java
@@ -3,6 +3,17 @@ package Algorithms.Spiral_matrix;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+ * This class in the solution for the leetcode problem "Spiral matrix":
+ * Given an m x n matrix, return all elements of the matrix in spiral order.
+ * 
+ * Constraints:
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 10
+ * -100 <= matrix[i][j] <= 100
+ */
+
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> answer = new LinkedList<>();
@@ -11,27 +22,6 @@ public class Solution {
         return answer;
     }
 
-    /*private void addSpiral(int x, int y, boolean[][] visited, int[][]  matrix, List<Integer> answer)
-    {
-        answer.add(matrix[x][y]);
-        visited[x][y] = true;
-        if( y < matrix[0].length - 1 && !visited[x][y + 1])
-        {
-            addSpiral(x, y + 1, visited, matrix, answer);
-        }else if( x < matrix.length - 1 && !visited[x + 1][y])
-        {
-            addSpiral(x + 1, y, visited, matrix, answer);
-        }else if( y > 0 && !visited[x][y - 1])
-        {
-            addSpiral(x , y - 1, visited, matrix, answer);
-        }else if( x > 0 && !visited[x - 1][y])
-        {
-            addSpiral(x - 1, y, visited, matrix, answer);
-        }else{
-            return;
-        }
-    }*/
-    
     private void addSpiral(int x, int y, boolean[][] visited, int[][]  matrix, List<Integer> answer, int dir)
     {
         answer.add(matrix[x][y]);
