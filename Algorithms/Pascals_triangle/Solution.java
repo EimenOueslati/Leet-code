0fdaeda209a4d073171c27ea6526e483d5134040
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-18 23:37:57 +0200

    Created the recursive function populateList to add the pascal

--- Algorithms/Pascals_triangle/Solution.java
+++ Algorithms/Pascals_triangle/Solution.java
@@ -6,13 +6,25 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> answer = new LinkedList<>();
-        if(numRows == 1) 
-        {
-            answer.add(new LinkedList<>());
+        answer.add(new LinkedList<>());
             answer.get(0).add(1);
+        if(numRows == 1) 
+        {  
             return answer;
         }
-
+        populateList(answer, 2, numRows);
         return answer;
+    }
+
+    private void populateList(List<List<Integer>> list, int index, int limit)
+    {
+        list.add(new LinkedList<>());
+        list.get(index - 1).add(1);
+        for(int i = 1; i < index - 1; i++)
+        {
+            list.get(index - 1).add(list.get(index - 2).get(i - 1) + list.get(index - 2).get(i));
+        }
+        if(index == limit) return;
+        else populateList(list, index++, limit);
     }
 }
