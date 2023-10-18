1cfb8bde31adc010e37a6003f7c0cc80e334c3cc
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-18 15:39:59 +0200

    Created the recursive function addSpiral

--- Algorithms/Spiral_matrix/Solution.java
+++ Algorithms/Spiral_matrix/Solution.java
@@ -6,9 +6,32 @@ import java.util.List;
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> answer = new LinkedList<>();
-        addSpiral(answer, matrix);
+        //addSpiral(answer, matrix);
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        answer.add(matrix[0][0]);
+        visited[0][0] = true;
         return answer;
     }
 
-    
+    private void addSpiral(int x, int y, boolean[][] visited, int[][]  matrix, List<Integer> answer)
+    {
+        answer.add(matrix[x][y]);
+        visited[x][y] = true;
+        if( y < matrix[0].length - 1 && !visited[x][y + 1])
+        {
+            addSpiral(x, y + 1, visited, matrix, answer);
+        }else if( x < matrix.length - 1 && !visited[x + 1][y])
+        {
+            addSpiral(x + 1, y, visited, matrix, answer);
+        }else if( y > 0 && !visited[x][y - 1])
+        {
+            addSpiral(x , y - 1, visited, matrix, answer);
+        }else if( x > 0 && !visited[x - 1][y])
+        {
+            addSpiral(x - 1, y, visited, matrix, answer);
+        }else{
+            return;
+        }
+    }
+
 }
