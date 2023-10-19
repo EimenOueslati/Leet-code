67dc57d1f611eee1af3dc14bd6b6ff3bfd9cd9ce
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-19 12:12:50 +0200

    Created the solution function for the Implement strStr() problem

--- Algorithms/Implement_strStr/SOlution.java
+++ Algorithms/Implement_strStr/SOlution.java
@@ -1,5 +1,18 @@
 package Algorithms.Implement_strStr;
 
-public class SOlution {
-    
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.equals(needle)) return 0;
+        if(needle.length() > haystack.length()) return -1;
+        for(int i = 0; i < haystack.length(); i++)
+        {
+            if(haystack.length() - i < needle.length()) return -1;
+            if(haystack.charAt(i) ==  needle.charAt(0))
+            {
+                if(haystack.substring(i, i + needle.length()).equals(needle)) return i;
+            }
+        }
+
+        return -1;
+    }
 }
