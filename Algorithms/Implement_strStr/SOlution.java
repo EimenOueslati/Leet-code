8b678852890f657a251d8b5933b2ede362d573f8
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-19 12:17:35 +0200

    Added a prblem description to the Implement strStr() solution class

--- Algorithms/Implement_strStr/SOlution.java
+++ Algorithms/Implement_strStr/SOlution.java
@@ -1,5 +1,15 @@
 package Algorithms.Implement_strStr;
 
+
+/*
+ * This class is the solution for the leetcode problem "Implement strStr()":
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ * 
+ * 
+ * Constraints:
+ * 1 <= haystack.length, needle.length <= 104
+ * haystack and needle consist of only lowercase English characters.
+ */
 class Solution {
     public int strStr(String haystack, String needle) {
         if(haystack.equals(needle)) return 0;
