ada766cbba7a38ee59e5b75ba3dafd312ad9740b
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-19 18:47:07 +0200

    Creates a test array to debug the solution class for the Longest Common Prefix class

---
+++ Algorithms/Longest_common_prefix/Main.java
@@ -0,0 +1,9 @@
+package Algorithms.Longest_common_prefix;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strs = {"anaanas", "anaanaszwin", "anaanasta3l7wa", "anaanasisbreedable", "anaanaszamelchwiya", "anaanasghannikko", "anaanasrapable"};
+        Solution sol = new Solution();
+        System.out.println(sol.longestCommonPrefix(strs));
+    }
+}
