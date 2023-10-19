a51ed833c0470534de8b79b55efed99a6de9408f
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-19 11:24:21 +0200

    Created a simple test for the Solution class for Add Binary problem

---
+++ Algorithms/Add_binary/Mian.java
@@ -0,0 +1,10 @@
+package Algorithms.Add_binary;
+
+public class Mian {
+    public static void main(String[] args) {
+        String op1 = "11";
+        String op2 = "1";
+        Solution sol = new Solution();
+        System.out.println(sol.addBinary(op1, op2));
+    }
+}
