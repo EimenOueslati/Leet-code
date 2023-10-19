50445ce0afd422645eb9dff4f8683b40f1367836
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-19 12:14:47 +0200

    Created a simple test for the Solution class for Implement strStr() problem

---
+++ Algorithms/Implement_strStr/Main.java
@@ -0,0 +1,10 @@
+package Algorithms.Implement_strStr;
+
+public class Main {
+    public static void main(String[] args) {
+        String hayStack = "sadbutsad";
+        String needle = "sad";
+        Solution sol = new Solution();
+        System.out.println(sol.strStr(hayStack, needle));
+    }
+}
