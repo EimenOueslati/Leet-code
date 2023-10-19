091e106fb6925819c75ab1ebcbb1d50b6e4a0801
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-19 11:15:09 +0200

    Created the function to perform the binary addition

--- Algorithms/Add_binary/Solution.java
+++ Algorithms/Add_binary/Solution.java
@@ -1,12 +1,12 @@
 package Algorithms.Add_binary;
 
 class Solution {
-    private int carryOver;
+    private char carryOver;
     public String addBinary(String a, String b) {
         int sa = a.length();
         int sb = b.length();
-        carryOver = 0;
-        StringBuilder answer;
+        carryOver = '0';
+        StringBuilder answer = new StringBuilder();
         StringBuilder op1;
         StringBuilder op2;
         if(sa >= sb)
@@ -24,7 +24,44 @@ class Solution {
         }
         for(int i = 0; i < op1.length(); i++)
         {
-            answer.append(addOperation(op1.charAt(i), op2.charAt(i), carryOver));
+            answer.insert(0, addOperation(op1.charAt(i), op2.charAt(i), carryOver));
+        }
+        if(carryOver == '1')  answer.insert(0, '1');
+        return answer.toString();
+    }
+
+    private char addOperation(char operand1, char operand2, int carryOver)
+    {
+        if(operand1 == '1' && operand2 == '1')
+        {  
+            if(carryOver == '0') 
+            {
+                carryOver = '1';
+                return '0';
+            }else
+            {
+                carryOver = '1';
+                return '1';
+            }
+        }else if(operand1 != operand2)
+        {
+             if(carryOver == '0') 
+            {
+                return '1';
+            }else
+            {
+                carryOver = '1';
+                return '0';
+            }
+        }else
+        {
+             if(carryOver == '0') 
+            {
+                return '0';
+            }else
+            {
+                return '1';
+            }
         }
     }
 }
