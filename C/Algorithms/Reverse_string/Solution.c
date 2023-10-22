b8ed98aa79412de8353300727056bd1da9e5ca6c
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-22 19:18:20 +0200

    Added some comments

--- C/Algorithms/Reverse_string/Solution.c
+++ C/Algorithms/Reverse_string/Solution.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-
+//This function is the solution for the leetcode problem "Revers String":
 void reverseString(char* s, int sSize){
+    //Created a char pointer to temporarily hold a vlue before swapping
     char *temp;
     int j = sSize - 1;
+    //Loop throught the String and swap the chars until we reach the middle.
     for(int i = 0; i < sSize / 2; i++)
     {
-       if(i == j) break;
+       if(i == j) break; // Break condition for when we reach the middle.
        temp = s[i];
        s[i] = s[j];
        s[j] = temp;
