e9ecddb41e481d55293c2422967af8226f84ee1b
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-22 19:16:11 +0200

    First Solution attempet for the Reverse Strign problem

--- C/Algorithms/Reverse_string/Solution.c
+++ C/Algorithms/Reverse_string/Solution.c
@@ -2,5 +2,14 @@
 
 
 void reverseString(char* s, int sSize){
-
-}
+    char *temp;
+    int j = sSize - 1;
+    for(int i = 0; i < sSize / 2; i++)
+    {
+       if(i == j) break;
+       temp = s[i];
+       s[i] = s[j];
+       s[j] = temp;
+       j--;
+    }
+}   
