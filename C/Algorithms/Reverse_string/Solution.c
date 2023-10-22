1ffdd957c76f863c0d38ba59c8558b140fc41cb2
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-22 19:19:28 +0200

    Added the problem desciption to the Reverse String problem

--- C/Algorithms/Reverse_string/Solution.c
+++ C/Algorithms/Reverse_string/Solution.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-//This function is the solution for the leetcode problem "Revers String":
+
+/*
+This function is the solution for the leetcode problem "Revers String":
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+ 
+*/
 void reverseString(char* s, int sSize){
     //Created a char pointer to temporarily hold a vlue before swapping
     char *temp;
