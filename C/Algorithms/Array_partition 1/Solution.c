0f052ebbda91ff34cbecb89f048ba23a4d5f970f
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-23 19:11:49 +0200

    First iteration of the solution for the Array Partition 1 problem

--- C/Algorithms/Array_partition 1/Solution.c
+++ C/Algorithms/Array_partition 1/Solution.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <limits.h>
 
 
 int arrayPairSum(int* nums, int numsSize){
-    printf(nums);
+    int sorted[numsSize];
+    int smallest = 0;
+    for(int i = 0; i < numsSize; i++)
+    {
+        for(int j = 0; j < numsSize; j++)
+        {
+            if(nums[j] <= nums[smallest])
+            {
+                smallest = j;
+            }
+        }
+        sorted[i] = nums[smallest];
+        nums[smallest] = INT_MAX;
+    }
+    int ret = 0;
+    for(int i = 0; i < numsSize; i += 2)
+    {
+        ret += sorted[i];
+    }
+    return sorted;
 }
