aaf0417edf0491be3578ddf7f349d29f155fb328
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-25 19:02:25 +0200

    Fixed another bug in the main loop

--- Java/Algorithms/Array_partition1/Solutiuon.java
+++ Java/Algorithms/Array_partition1/Solutiuon.java
@@ -6,7 +6,7 @@ public class Solutiuon {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums); 
         int answer = 0;
-        for(int i = 0; i <= nums.length / 2; i += 2)
+        for(int i = 0; i < nums.length - 1; i += 2)
         {
             answer += nums[i];
         }
