7bd574c0e5326c563efbbf507b501894fa3e60fa
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-25 19:00:34 +0200

    Created another test for the solution of Array Partition 1

--- Java/Algorithms/Array_partition1/Main.java
+++ Java/Algorithms/Array_partition1/Main.java
@@ -2,7 +2,7 @@ package Algorithms.Array_partition1;
 
 public class Main {
     public static void main(String[] args) {
-        int[] test = {1,4,3,2};
+        int[] test = {6,2,6,5,1,2};
         Solutiuon sol = new Solutiuon();
         System.out.println(sol.arrayPairSum(test));
     }
