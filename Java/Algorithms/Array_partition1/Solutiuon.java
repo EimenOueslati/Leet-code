562d04b7c025ca9a58daa34cf4b0561fbc702e65
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-25 18:55:09 +0200

    Implemented a solution for the Array Partition 1 problem

---
+++ Java/Algorithms/Array_partition1/Solutiuon.java
@@ -0,0 +1,15 @@
+package Algorithms.Array_partition1;
+
+import java.util.Arrays;
+
+public class Solutiuon {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums); 
+        int answer = 0;
+        for(int i = 0; i < nums.length / 2; i += 2)
+        {
+            answer += nums[i]
+        }
+        return answer;
+    }
+}
