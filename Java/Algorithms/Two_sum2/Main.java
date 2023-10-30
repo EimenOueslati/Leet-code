e535fbd84377c1a476128808ce05e9505d19a849
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-30 21:44:00 +0100

    Created a different  test for the Solution class for Two sum 2 problem

--- Java/Algorithms/Two_sum2/Main.java
+++ Java/Algorithms/Two_sum2/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] test = {2,3,4};
+        int[] test = {2,7,11,15};
         Solution sol = new Solution();
         System.out.println(Arrays.toString(sol.twoSum(test, 6)));
     }
