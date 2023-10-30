4750e7b8fab8a83122e9923efa2e47f941366169
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-30 21:08:33 +0100

    Switching to a binary search solution using recursion

--- Java/Algorithms/Two_sum2/Solution.java
+++ Java/Algorithms/Two_sum2/Solution.java
@@ -4,15 +4,10 @@ public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] ret = new int[2];
         int x;
+        int half;
         for(int i = 0; i < numbers.length; i++)
         {
-            for(int j = i + 1; j < numbers.length; j++)
-            {
-                if(numbers[i] + numbers[j] == target) 
-                {
-                    ret = new int[]{i + 1, j + 1};
-                }
-            }
+             half = (int)((numbers.length - 1) / 2); 
         }
         
         return ret;
