5fdb1557992d22cd49386d68b945353b2f982d43
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-10-30 21:15:37 +0100

    Created the recursive function

--- Java/Algorithms/Two_sum2/Solution.java
+++ Java/Algorithms/Two_sum2/Solution.java
@@ -2,14 +2,33 @@ package Algorithms.Two_sum2;
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] ret = new int[2];
-        int x;
-        int half;
-        for(int i = 0; i < numbers.length; i++)
+        for(int i = 0; i < numbers.length - 1; i++)
         {
-             half = (int)((numbers.length - 1) / 2); 
+            int temp = binSearch(numbers, numbers[i], target);
+            if( temp != -1) return new int[]{i, temp};
         }
-        
-        return ret;
+        return null;
+    }
+
+    private int binSearch(int[] arr, int i, int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+        int curr = arr.length/2;
+        while(start != end)
+        {
+            if(arr[curr] + i ==  target) return curr;
+            else if(arr[curr] + i < target)
+            {
+                start = curr;
+                curr = (start + end) / 2;
+            }
+            else
+            {
+                end = curr;
+                curr = (start + end) / 2;
+            }
+        }
+        return -1;
     }
 }
