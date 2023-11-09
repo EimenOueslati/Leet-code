df13dde4e3937ca173036b6c0583053db418a592
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-11-09 13:28:16 +0100

    Added a prblem description to the Remove Element solution class

--- Java/Algorithms/Remove_elements/Solution.java
+++ Java/Algorithms/Remove_elements/Solution.java
@@ -1,5 +1,19 @@
 package Algorithms.Remove_elements;
 
+
+
+/*
+ * This class is the solution for the leetcode problem "Remove element":
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ * Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+ * Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+ * Return k.
+ * 
+ * Constraints:
+ * 0 <= nums.length <= 100
+ * 0 <= nums[i] <= 50
+ * 0 <= val <= 100
+ */
 public class Solution {
     public int removeElement(int[] nums, int val) {
         int index = 0;
