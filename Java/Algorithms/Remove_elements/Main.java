2354b9f4eaab1e053708adc9239436d967bbb198
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-11-09 13:23:12 +0100

    Created a simple test for the Solution class for Remove Element problem

--- Java/Algorithms/Remove_elements/Main.java
+++ Java/Algorithms/Remove_elements/Main.java
@@ -1,5 +1,13 @@
 package Algorithms.Remove_elements;
 
+import java.util.Arrays;
+
 public class Main {
+     public static void main(String[] args) {
+        int[] test = {2,4,5,66,45,7,4,5,2,54,2,57,2,2,27,575,57,2,2,78,2};
+        Solution sol = new Solution();
+        System.out.println(sol.removeElement(test, 2));
+        System.out.println(Arrays.toString(test));
+    }
 
 }
