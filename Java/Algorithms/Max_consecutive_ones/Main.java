959e60e6ef61d6a2b8a88bfa219c3fd759b73698
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-11-10 22:28:12 +0100

    Created a different  test for the Solution class for Max Consecutive Ones problem

--- Java/Algorithms/Max_consecutive_ones/Main.java
+++ Java/Algorithms/Max_consecutive_ones/Main.java
@@ -2,7 +2,7 @@ package Algorithms.Max_consecutive_ones;
 
 public class Main {
     public static void main(String[] args) {
-        int[] test = {1,1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        int[] test = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
         Solution sol = new Solution();
         System.out.println(sol.findMaxConsecutiveOnes(test));
     }
