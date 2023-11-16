1cbd440dd892717a8d4f50dbf682ec551c4a2499
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-11-16 16:26:13 +0100

    Created a simple test for the Solution class for Minimum size subarray sum problem

---
+++ Java/Algorithms/Minimum_size_subarray_sum/Main.java
@@ -0,0 +1,9 @@
+package Algorithms.Minimum_size_subarray_sum;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] test = {1,2,3,4,5};
+        Solution sol = new Solution();
+        System.out.println(sol.minSubArrayLen(11, test));
+    }
+}
