d08065cd2ab245c85ce9fbe4a4e755d8c2c76036
Eimen Ouesalti <aymanoueslati22@gmail.com>
2023-11-16 16:24:02 +0100

    Created the main function to get the subarray length

--- Java/Algorithms/Minimum_size_subarray_sum/Solution.java
+++ Java/Algorithms/Minimum_size_subarray_sum/Solution.java
@@ -4,7 +4,17 @@ import java.util.Arrays;
 
 public class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        return 0;
+        int curr = Integer.MAX_VALUE;
+        int[] temp;
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            for(int j = i + 1; j <  nums.length + 1; j++)
+            {
+                temp = Arrays.copyOfRange(nums, i, j);
+                if(sumarray(temp) == target && temp.length < curr) curr = temp.length;
+            }
+        }
+        return curr == Integer.MAX_VALUE ? 0 : curr;
     }
 
     private int sumarray(int[] arr)
