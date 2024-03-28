21c984426e67c50ee64a2b16df64fa5b2b3832dc
Eimen Ouesalti <aymanoueslati22@gmail.com>
2024-03-28 08:50:03 +0100

    solution for Find Numbers with Even Number of Digits roblem

---
+++ C++/Algorithms/Find Numbers with Even Number of Digits/solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cmath>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for(int num : nums){
+            if(floor(log10(n) + 1) % 2 != 0){
+                count++;
+            }
+        }
+        
+    }
+};
