30f3a4a892f8f2abcefd342815fdad42e9cd8a8d
Eimen Ouesalti <aymanoueslati22@gmail.com>
2024-03-28 09:54:27 +0100

    Corrected typos in problem description

--- C++/Algorithms/Max consecutive ones/solution.cpp
+++ C++/Algorithms/Max consecutive ones/solution.cpp
@@ -5,7 +5,7 @@ using namespace std;
 /*
     Given a binary array nums, return the maximum number of consecutive 1's in the array.
     Constraints:
-    1 <= nums.length <= 10
+    1 <= nums.length <= 10*5
     nums[i] is either 0 or 1.
 */
 class Solution {
--- C++/Algorithms/squares of a sorted array/solution.cpp
+++ C++/Algorithms/squares of a sorted array/solution.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 /*
     Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
-    
+
     Constraints:
-    1 <= nums.length <= 104
-    -104 <= nums[i] <= 104
+    1 <= nums.length <= 10**4
+    -10**4 <= nums[i] <= 10**4
     nums is sorted in non-decreasing order.
 */
 class Solution {
